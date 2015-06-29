29e50902a1d68b2ffdc354a97548c276a35b19bc
anvaka <anvaka@gmail.com>
2015-06-28 18:56:32 -0700

    Updated woding

--- toBinary.js
+++ toBinary.js
@@ -3,4 +3,4 @@ var save = require('ngraph.tobinary');
 save(graph, { outDir: './data' });
 
 console.log('Done.');
-console.log('Copy `links.bin`, `labels.bin` and last position file (e.g. `./data/100.pos`) into vis folder');
+console.log('Copy `links.bin`, `labels.bin` and positions.bin into vis folder');
